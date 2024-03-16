33475a28d51028906538bb104ac5a3a56382f7e6
peacetrue <xiayouxue@gmail.com>
2024-03-17 00:20:01 +0800

    探索主题之实现原理

--- peace-mui/src/App.tsx
+++ peace-mui/src/App.tsx
@@ -1,11 +1,43 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import {Button, createTheme, CssBaseline, ThemeProvider} from '@mui/material';
 
 function App() {
+  // https://mui.com/material-ui/customization/default-theme/
+  let theme = createTheme({
+    components: {
+      MuiButton: {
+        styleOverrides: {
+          root: ({theme}) => ({
+            color: 'rgba(0,0,255,1)'
+          })
+        }
+      }
+    },
+    palette: {
+      // mode: 'dark',
+      primary: {
+        main: 'rgba(0,0,0,1)'
+      },
+      text: {
+        primary: 'rgba(0,255,0,1)'
+      },
+      background: {
+        default: 'rgba(255,0,0,1)'
+      }
+    },
+    typography: {
+      fontSize: 24,
+    }
+  });
   return (
-    <Box>
-      Hello MUI!
-    </Box>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <Button variant={'contained'}
+              color={'primary'}
+              sx={{color: 'rgba(255,255,255,1)'}}>
+        Hello MUI!
+      </Button>
+    </ThemeProvider>
   );
 }
 
